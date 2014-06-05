b9f8591103ba2108c7aad452d8aeb2a4b8e44569
Poojan Shrestha <poojans@gmail.com>
2014-06-05 10:56:21 +0545

    Added scaffoldery generate command.

---
+++ scaffold_templates/johndoe/scaffold-angular-controller/controller.js
@@ -0,0 +1,5 @@
+angular.module('%module%', [])
+  .controller('%name%', ['$scope', function($scope) {
+    // TODO
+  }])
+
---
+++ scaffold_templates/johndoe/scaffold-angular/directive.js
@@ -0,0 +1,8 @@
+angular.module('%module%', []).directive('%name%', function(){
+
+    return {
+        template: 'This is an example',
+        restrict: 'E'
+    }
+
+});
