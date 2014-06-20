4e2e235bc845ae2d9dce335fa8e7d22b3c60cf89
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-06-20 13:54:11 +0200

    fix test

--- test/scaffoldery-init.spec.js
+++ test/scaffoldery-init.spec.js
@@ -1,26 +1,25 @@
 'use strict';
 /* global require, ok, __dirname, describe, it */
 
-var exec = require('child_process').exec;
-var path = require('path');
-var fs = require('fs');
-var should = require('should');
-var bin = __dirname + '/../bin/scaffoldery';
+var exec = require('child_process').exec,
+  path = require('path'),
+  fs = require('fs'),
+  should = require('should'),
+  bin = __dirname + '/../bin/scaffoldery';
 
 describe('scaffoldery init', function () {
   it('should generate scaffoldery.json', function (done) {
-    exec('cd test/tmp && ' + bin + ' init --name foo --platform javascript', function (err, stdout) {
+    exec('cd test/tmp && ' + bin + ' init --name foo --platform javascript', function (err) {
       if (err) return done(err);
 
-      fs.existsSync('test/tmp/scaffoldery.json').should.be.ok;
       fs.readFile('test/tmp/scaffoldery.json', function (err, data) {
         if (err) throw err;
         data.toString().should.containEql('name');
         data.toString().should.containEql('foo');
         data.toString().should.containEql('platform');
         data.toString().should.containEql('javascript');
+        done();
       });
-      done();
     });
   });
 });
